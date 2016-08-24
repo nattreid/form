ab118289d5896eabe23f489c1b58884522c73318
Attreid <attreid@gmail.com>
2016-08-24 14:45:56 +0200

    nextras js

--- assets/form.js
+++ assets/form.js
@@ -1,4 +1,4 @@
-(function ($, window) {
+(function ($, window, Nette) {
     if (window.jQuery === undefined) {
         console.error('Plugin "jQuery" required by "form.js" is missing!');
         return;
@@ -153,6 +153,34 @@
         }
     });
 
-})(jQuery, window);
+    // nextras form
+    Nette.getValuePrototype = Nette.getValue;
+    Nette.getValue = function (elem) {
+        if (!elem || !elem.nodeName || !(elem.nodeName.toLowerCase() == 'input' && elem.name.match(/\[\]$/))) {
+            return Nette.getValuePrototype(elem);
+        } else {
+            var value = [];
+            for (var i = 0; i < elem.form.elements.length; i++) {
+                var e = elem.form.elements[i];
+                if (e.nodeName.toLowerCase() == 'input' && e.name == elem.name && e.checked) {
+                    value.push(e.value);
+                }
+            }
+
+            return value.length == 0 ? null : value;
+        }
+    };
+
+    // typehead
+    $('.typeahead').each(function () {
+        $(this).typeahead({
+            remote: {
+                url: $(this).attr('data-typeahead-url'),
+                wildcard: '__QUERY_PLACEHOLDER__'
+            }
+        });
+    });
+
+})(jQuery, window, Nette);
 
 
