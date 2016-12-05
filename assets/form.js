b529f20cdd6d35b00031ebd7c5e6f8d016b7e3d9
attreid <attreid@gmail.com>
2016-12-05 15:01:05 +0100

    uprava validatoru

--- assets/form.js
+++ assets/form.js
@@ -190,6 +190,9 @@
 
 })(jQuery, window, Nette);
 Nette.validators.NAttreidFormRules_validatePhone = function (elem, arg, value) {
+    if (!elem.hasAttribute('required') && value.length === 0) {
+        return true;
+    }
     var regexp = /^(\(?\+?([0-9]{1,4})\)?)?([0-9]{6,16})$/;
     return regexp.test(value.replace(/[-\.\s]+/g, ''));
 };
--- resources/assets/js/rules.js
+++ resources/assets/js/rules.js
@@ -1,4 +1,7 @@
 Nette.validators.NAttreidFormRules_validatePhone = function (elem, arg, value) {
+    if (!elem.hasAttribute('required') && value.length === 0) {
+        return true;
+    }
     var regexp = /^(\(?\+?([0-9]{1,4})\)?)?([0-9]{6,16})$/;
     return regexp.test(value.replace(/[-\.\s]+/g, ''));
 };
