3cc477732fa1b15a10aab92ba0eba4cba3ac78cd
attreid <attreid@gmail.com>
2017-06-01 09:45:05 +0200

    uprava lokalizace

--- assets/form.js
+++ assets/form.js
@@ -22,7 +22,7 @@
     var localize = {
         cs: {
             locale: {
-                applyLabel: 'Odeslat',
+                applyLabel: 'Vložit',
                 cancelLabel: 'Zrušit',
                 fromLabel: 'Od',
                 toLabel: 'Do',
